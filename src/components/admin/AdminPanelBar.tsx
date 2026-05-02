47d1a67f2dcdf91146e4b0d84d904e3146fa120a
tutuxka <147097423+rabbits0209@users.noreply.github.com>
2026-05-02 14:37:03 +0800

    Gx

--- src/components/admin/AdminPanelBar.tsx
+++ src/components/admin/AdminPanelBar.tsx
@@ -173,7 +173,7 @@ const AdminPanelBar = ({ content }: AdminPanelBarProps) => {
     async function loadReleases() {
       try {
         const resp = await fetch(
-          "https://api.github.com/repos/komari-monitor/komari/releases?per_page=100",
+          "https://api.github.com/repos/rabbits0209/komari/releases?per_page=100",
           {
             headers: {
               Accept: "application/vnd.github+json",
